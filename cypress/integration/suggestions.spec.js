fb24c875ed1ebe8d474a8adb4bae33057ab3335e
JKatzwinkel <JKatzwinkel@users.noreply.github.com>
2023-04-16 16:25:26 +0200

    add second text input field for comparison

--- cypress/integration/suggestions.spec.js
+++ cypress/integration/suggestions.spec.js
@@ -1,4 +1,13 @@
-it('type in input field with suggestions', () => {
-  cy.visit('/')
-  cy.get('#field1').type('foo')
+describe('input fields with datalist suggestions', () => {
+
+  it('typing into field with static suggestions', () => {
+    cy.visit('/')
+    cy.get('#field2').type('lo')
+  })
+
+  it('typing into field making AJAX call', () => {
+    cy.visit('/')
+    cy.get('#field1').type('foo')
+  })
+
 })
